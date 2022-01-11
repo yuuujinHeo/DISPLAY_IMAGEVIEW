2829cb367b0a5bbe8482118b59f5fa76ae729294
yujin heo <yjheo4@rainbow-robotics.com>
2022-01-11 14:20:45 +0900

    imageview..

---
+++ OneView.qml
@@ -0,0 +1,25 @@
+﻿import QtQuick 2.0
+import QtQuick.Window 2.3
+import QtQuick.Controls 2.3
+import QtQuick.Layouts 1.0
+
+Window {
+    visible: true
+    width: 1920
+    height: 1080
+    visibility: "FullScreen"
+
+    Image{
+        id: imageBackground
+        anchors.fill: parent
+        source: "image/image.png"
+    }
+}
+
+
+
+/*##^##
+Designer {
+    D{i:0;formeditorZoom:0.25}
+}
+##^##*/
---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlComponent>
+#include <QCursor>
+
+QObject *object;
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    app.setOverrideCursor(QCursor(Qt::BlankCursor));
+
+    QQmlEngine engine;
+    QQmlComponent component(&engine, "qrc:/OneView.qml");
+    object = component.create();
+
+    return app.exec();
+}
